2415c29e1218a8266389107b6cf6eaf0abdee4cb
Insanitogames <67851958+Insanitogames@users.noreply.github.com>
2020-09-07 16:26:05 +1200

    Matty

--- commands/tictaktoe.js
+++ commands/tictaktoe.js
@@ -5,8 +5,8 @@ module.exports.run = async (client, message, args) => {
     const ttt = require("discord.js-tictactoe")
     const embed_color = "#ff00aa"
     const start_cmd = "ttt"
-    ttt.run(client, embed_color, start_cmd)
-    
+    message.channel.send(ttt.run(embed_color, start_cmd))
+
 }
   
 module.exports.help = {
