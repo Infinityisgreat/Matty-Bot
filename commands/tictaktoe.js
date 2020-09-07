2484afd5f3a5bc497750a1caf9d9f86b2e0ce2c4
Insanitogames <67851958+Insanitogames@users.noreply.github.com>
2020-09-07 16:02:01 +1200

    Matty

---
+++ commands/tictaktoe.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const fs = require('fs');
+
+module.exports.run = async (client, message, args) => {
+    const ttt = require("discord.js-tictactoe")
+    const prefix = "!"
+    const embed_color = "#ff00aa"
+    const start_cmd = "ttt"
+    ttt.run(client, prefix, embed_color, start_cmd)
+    
+}
+  
+module.exports.help = {
+    name: 'ttc',
+    aliases: []
+  };
